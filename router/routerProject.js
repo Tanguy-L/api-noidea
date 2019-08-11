085bd3eecc8197d76678896db9d4f66e9b5799b6
Tanguy <tanguy.leg@gmail.com>
2019-08-11 23:56:34 +0200

    update router

--- router/routerProject.js
+++ router/routerProject.js
@@ -6,7 +6,7 @@ module.exports = ({router}) => {
   router.get('/throwerror', (ctx, next) => {
     throw new Error('Aghh! An error!');
   });
-  router.post('/v1/projects/create', project.createProject);
+  router.post('/v1/projects', project.createProject);
   router.delete('/v1/projects/:id', project.deleteProject);
   router.put('/v1/projects/:id', project.updateProject);
 };
